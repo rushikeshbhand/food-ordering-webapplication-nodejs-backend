120f7bca3a544f55a4b36a8c9610351e1e9c4b40
Rushikesh Bhand <112516622+rushikeshbhand@users.noreply.github.com>
2024-06-15 11:05:01 +0530

    signup and login is done without bcrypt and jwt

--- models/userModel.js
+++ models/userModel.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
+    password: {type: String, minlength:4, maxlength:25, required: true},
     email: {type: String, required: true, unique: true},
-    createdAt: { type: Date, default: Date.now()}
+    createdAt: { type: Date, default: Date.now}
 })
 
 const userModel = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -1,10 +1,58 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+require('dotenv').config(); // Load dotenv configuration
+require('./dbConnect'); 
+const user = require('./models/userModel')
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middleware to parse URL-encoded bodies (form data)
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hey hello welcome to authapp');
+});
+
+// signup form 
+app.post('/createUser', async (req, res)=>{
+  try{
+    const { username, password, email} = req.body;
+    const newUser = new user({
+      username,
+      password,
+      email
+    })
+    const createdUser = await newUser.save()
+    res.status(201).send(createdUser)
+  }
+  catch(error){
+    console.log(error)
+    res.status(400).send(error)
+  }
 })
 
-app.listen(5000, () => {
-  console.log('Example app listening on port 5000!')
+// login form 
+app.post('/login', async (req, res)=>{
+  try{
+    const {email, password} = req.body
+    const userFound = await user.findOne({email:email, password:password})
+    if(userFound){
+      res.status(200).send( userFound)
+      console.log(userFound)
+    }
+    else{
+      res.status(404).send('User not found')
+    }
+  }
+  catch(error){
+    console.log(error)
+    res.status(400).send(error)
+  }
 })
+
+app.listen(port, () => {
+  console.log('Example app listening on port ' + port);
+});
