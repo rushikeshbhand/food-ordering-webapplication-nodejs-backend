5b2be597ee08670562d45fd5383dceed3efb250e
Rushikesh Bhand <112516622+rushikeshbhand@users.noreply.github.com>
2024-06-30 18:28:12 +0530

    const was missing in user model so i add const there

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, minlength:4, required: true},
     email: {type: String, required: true, unique: true},
